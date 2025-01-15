7e7a26d21ebabf4b612d2bd72c374503da7bfb1b
Bruno Penner <brunopenner01@gmail.com>
2025-01-15 09:13:47 +0800

    #108 splitting navbar into multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -50,8 +50,48 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-export default function App() {
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+        <Logo />
+        <Search />
+        <NumResults />
+      </nav>
+  )
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+  )
+}
+
+function Search() {
   const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  )
+}
+
+function NumResults() {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  )
+}
+
+function Main() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen1, setIsOpen1] = useState(true);
@@ -62,25 +102,7 @@ export default function App() {
   const avgRuntime = average(watched.map((movie) => movie.runtime));
 
   return (
-    <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
-
-      <main className="main">
+    <main className="main">
         <div className="box">
           <button
             className="btn-toggle"
@@ -163,6 +185,15 @@ export default function App() {
           )}
         </div>
       </main>
+  )
+}
+
+export default function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Main />
     </>
   );
 }
